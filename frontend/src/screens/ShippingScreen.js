384c6d96ad255f667f181893c9c53a112299926a
hamzamughal <mughalh473@gmail.com>
2021-08-19 23:46:05 +0500

    place order screen added

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { saveShippingAddress } from '../actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
 import FormContainer from '../components/FormContainer'
+import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
 
