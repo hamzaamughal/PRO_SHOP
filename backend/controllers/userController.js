0029afc1af62b2c1642943eeb956a7af62249870
hamzamughal <mughalh473@gmail.com>
2021-08-19 22:01:43 +0500

    front-end user auth completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,32 +42,6 @@ const getUserProfile = expressAsyncHandler(async (req, res) => {
         throw new Error('User not found')
     }
 })
-// @desc      update user profile
-// @route     PUT /api/users/profile
-// @access    Private
-const updateUserProfile = expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id)
-
-    if (user) {
-        user.name = req.body.name || user.name
-        user.email = req.body.email || user.email
-        if (req.body.password) {
-            user.password = req.body.password
-        }
-        const updatedUser = await user.save()
-
-        res.json({
-            _id: updatedUser._id,
-            name: updatedUser.name,
-            email: updatedUser.email,
-            isAdmin: updatedUser.isAdmin,
-            token: generateToken(updatedUser._id)
-        })
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-    }
-})
 
 // @desc      Register a new user
 // @route     POST /api/users
@@ -98,5 +72,34 @@ const registerUser = expressAsyncHandler(async (req, res) => {
         throw new Error('Invalid user data')
     }
 })
+
+// @desc      update user profile
+// @route     PUT /api/users/profile
+// @access    Private
+const updateUserProfile = expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+
 
 export { authUser, getUserProfile, registerUser, updateUserProfile }
