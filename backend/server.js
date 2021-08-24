b099f717292bbf41097c660a72ccd33ae2784efe
hamzamughal <mughalh473@gmail.com>
2021-08-24 22:22:44 +0500

    file uploading done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express'
 import dotenv from 'dotenv';
 import cors from 'cors'
@@ -9,6 +10,7 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 
 dotenv.config()
@@ -22,9 +24,12 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use(notFound)
 app.use(errorHandler)
 
