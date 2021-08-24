466a731ed53229b00a766c1a88f13335400d8789
hamzamughal <mughalh473@gmail.com>
2021-08-24 14:29:42 +0500

    delete product by admin functionality added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,63 @@ const getProductById = expressAsyncHandler(async (req, res) => {
     }
 })
 
-export { getProductById, getProducts }
+// @desc      Delete a Product
+// @route     DELETE /api/products/:id
+// @access    Public / Admin
+const deleteProduct = expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await product.remove()
+        res.json({ message: 'Product Removed' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+
+// @desc      Create a Product
+// @route     POST /api/products/:id
+// @access    Public / Admin
+const createProduct = expressAsyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample Brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample Description'
+    })
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+})
+
+// @desc      Update a Product
+// @route     PUT /api/products/:id
+// @access    Public / Admin
+const updateProduct = expressAsyncHandler(async (req, res) => {
+    const { name, price, description, image, brand, category, countInStock } = req.body
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.status(201).json(updatedProduct)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+
+
+export { getProductById, getProducts, deleteProduct, createProduct, updateProduct }
