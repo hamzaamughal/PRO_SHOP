862d9bb3b865c1969f7bbc68177d70fe818f72fc
hamzamughal <mughalh473@gmail.com>
2021-08-25 18:06:01 +0500

    pagination completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,9 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/' exact component={HomeScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} />
+          <Route path='/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword/page/:pageNumber' component={HomeScreen} exact />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/order/:id' component={OrderScreen} />
           <Route path='/login' component={LoginScreen} />
@@ -37,7 +39,8 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/productlist' component={ProductListScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} exact />
+          <Route path='/admin/productlist/:pageNumber' component={ProductListScreen} exact />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,11 +2,11 @@ import { PRODUCT_CREATE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_REVIEW_FAIL
 import axios from 'axios';
 
 
-export const listProducts = (keyword = '') => async (dispatch) => {
+export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get(`/api/products?keyword=${keyword}`)
+        const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
             payload: data
